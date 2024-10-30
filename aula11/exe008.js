0027a1db28e4c8279e2f26552b8f5f5cef0e1802
ClauGian <cgclaugian@gmail.com>
2024-10-29 23:33:51 -0300

    Exerecicios

---
+++ aula11/exe008.js
@@ -0,0 +1,9 @@
+var vel = 90
+
+console.log (`Você estava a ${vel} km/h.`)
+
+if (vel > 80) {
+    console.log (`Ultrapassou o limite de velocidade. SERÁ MULTADO`)
+}
+
+console.log ('Não se esqueça de usar o cinto de segurança.')
