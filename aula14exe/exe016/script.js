ab7f26549856ca24ea1f802f1fce308906afa2ad
ClauGian <cgclaugian@gmail.com>
2024-11-01 17:11:10 -0300

    Exercicio repetição for

--- aula14exe/exe016/script.js
+++ aula14exe/exe016/script.js
@@ -1,11 +1,37 @@
-
 function contar() {
-    var inicio = document.querySelector('input#iinicio')
-    var ifim = document.querySelector('input#fifim')
-    var ipasso = document.querySelector('input#pipasso')
-    var res = document.querySelector('div#resp')   
-    var ini = Number(inicio.value)
-    var fim = Number(ifim.value)
-    var passo = Number(ipasso.value)
+
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
     
-} 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('ERRO, faltam dados')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if (p <= 0){
+            window.alert('Passo invalido. Considerando passo (1)')
+            p = 1
+        }
+
+        if (i < f){
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+            res.innerHTML += ` \u{1f3c1}` 
+        } else {
+            for ( let c = i;  c >= f;  c -= p){
+                res.innerHTML += ` ${c} \u{1f92f}  `
+            }
+            res.innerHTML += ` \u{1f3c1}` 
+        }
+        
+            
+    }
+        
+   
+}
