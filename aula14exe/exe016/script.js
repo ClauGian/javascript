674d8e9c138c03404ed4fddcf8d60a0f34580479
ClauGian <cgclaugian@gmail.com>
2024-10-31 23:00:30 -0300

    tentando

---
+++ aula14exe/exe016/script.js
@@ -0,0 +1,11 @@
+
+function contar() {
+    var inicio = document.querySelector('input#iinicio')
+    var ifim = document.querySelector('input#fifim')
+    var ipasso = document.querySelector('input#pipasso')
+    var res = document.querySelector('div#resp')   
+    var ini = Number(inicio.value)
+    var fim = Number(ifim.value)
+    var passo = Number(ipasso.value)
+    
+} 
