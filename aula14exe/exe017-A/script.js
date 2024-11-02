b78e1b0e163b8c85c578db0379321f591a791d98
ClauGian <cgclaugian@gmail.com>
2024-11-02 10:21:18 -0300

    mais uma tabuada

--- aula14exe/exe017-A/script.js
+++ aula14exe/exe017-A/script.js
@@ -2,11 +2,11 @@ function gerar(){
 
     var num = parseInt(document.getElementById("num").value);
     var resposta = document.getElementById('resposta');
+   
     var gerar='';
     
-    for(var c = 1; c <= 10 ; c ++)
-        gerar +=num+" x "+c+" = "+num*c+"<br />";
-    
+    for(var c = 1; c <= 10 ; c ++)        
+        gerar +=num+" x "+c+" = "+num*c+"<br />";    
         resposta.innerHTML = gerar;
         
   }
