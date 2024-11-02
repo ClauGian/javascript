832022a2124bb2f41e4b165f10b8d10d2238a47a
ClauGian <cgclaugian@gmail.com>
2024-11-01 22:12:09 -0300

    Exercicio tabuada

---
+++ aula14exe/exe017-A/script.js
@@ -0,0 +1,13 @@
+function gerar(){
+
+    var num = parseInt(document.getElementById("num").value);
+    var resposta = document.getElementById('resposta');
+    var gerar='';
+    
+    for(var c = 1; c <= 10 ; c ++)
+        gerar +=num+" x "+c+" = "+num*c+"<br />";
+    
+        resposta.innerHTML = gerar;
+        
+  }
+  
