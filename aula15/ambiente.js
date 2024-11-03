0f07bcd2abf7c04c3c4f581688bb91449a689fff
ClauGian <cgclaugian@gmail.com>
2024-11-03 11:10:13 -0300

    vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,9 @@
-let num = [5, 4, 9, 8, 2]
-
-console.log(`Nosso vetor é: ${num}`)
-console.log(num)
+let num = [5, 8, 4]
+console.log('let num = [5, 8, 4]')
+num[3]=6
+console.log(`num[3]=6 [${num}]`)
+num.push(7)
+console.log(`num.push(7) [${num}]`)
+num.sort()
+console.log(`num.sort() [${num}]`)
+console.log([num.length])
---
+++ aula15/vetor.js
@@ -0,0 +1,9 @@
+console.log('-----------------')
+let valores = [5, 8, 9, 4, 2, 7, 3]
+console.log('----------------')
+console.log(`esses são os valores do vetor ${valores}`)
+console.log('----------------')
+
+for (let pos = 0; pos <= 6; pos++){
+    console.log(`O valor da posição ${pos} é igual a ${valores[pos]}`)
+}
